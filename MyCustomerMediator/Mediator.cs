0bc148856a182f8ed6a7e67db5ad584e1bd721ee
Tayyab94 <tayyab.bhatti30@gmail.com>
2025-04-19 21:16:42 +0500

    first commit

---
+++ MyCustomerMediator/Mediator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCustomerMediator
+{
+    public class Mediator(IServiceProvider _serviceProvider)
+    {
+
+        //public async Task<TResponse> Send<TRequest, TResponse>(TRequest request, CancellationToken cancellationToken = default) where TRequest : IRequest<TResponse>
+        //{
+        //    var handler = _serviceProvider.GetService(typeof(IRequestHandler<TRequest, TResponse>)) as IRequestHandler<TRequest, TResponse>;
+        //    if (handler == null)
+        //    {
+        //        throw new InvalidOperationException($"Handler not found for request type {typeof(TRequest).Name}");
+        //    }
+        //    return await handler.Handle(request, cancellationToken);
+        //}
+
+        public async Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+        {
+            var handlerType=typeof(IRequestHandler<,>).MakeGenericType(request.GetType(),typeof(TResponse));
+            dynamic handler =_serviceProvider.GetService(handlerType);
+            if (handler == null)
+                throw new InvalidOperationException($"Handler for {request.GetType().Name} not found");
+
+            return await handler.Handle((dynamic)request, cancellationToken);
+        }
+    }
+}
---
+++ MyCustomerMediator/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCustomerMediator
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMyMediator(this IServiceCollection services,
+            params Assembly[] assemblies)
+        {
+            services.AddScoped<Mediator>();
+            var handlerInterfaceType = typeof(IRequestHandler<,>);
+
+            var handlers = assemblies.SelectMany(assembly => assembly.GetTypes())
+                .Where(t => !t.IsAbstract && !t.IsInterface)
+                .SelectMany(t => t.GetInterfaces()
+                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == handlerInterfaceType),
+                (t, i) => new { HandlerType = t, InterfaceType = i });
+
+            foreach (var hanlder in handlers)
+            {
+                services.AddScoped(hanlder.InterfaceType, hanlder.HandlerType);
+            }
+            return services;
+        }
+    }
+}
