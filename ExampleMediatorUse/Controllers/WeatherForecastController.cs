d5ebfd22d7a829dfe5fadc60f133658357ea861b
Tayyab94 <tayyab.bhatti30@gmail.com>
2025-04-19 21:26:11 +0500

    implementation mycustomMediator

---
+++ ExampleMediatorUse/Controllers/WeatherForecastController.cs
@@ -0,0 +1,21 @@
+﻿using ExampleMediatorUse.Features.Queries;
+using Microsoft.AspNetCore.Mvc;
+using MyCustomerMediator;
+
+namespace ExampleMediatorUse.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController(Mediator _mediator): ControllerBase
+    {
+
+        [HttpGet("welcome")]
+        public async Task<IActionResult>GetWelcomMessage([FromQuery]string name)
+        {
+            var query = new WelcomMessageQuery { Name= name };
+
+            var reqult = await _mediator.Send(query);
+            return Ok(reqult);
+        }
+    }
+}
---
+++ ExampleMediatorUse/Features/Queries/WelcomMessageQuery.cs
@@ -0,0 +1,17 @@
+﻿using MyCustomerMediator;
+
+namespace ExampleMediatorUse.Features.Queries
+{
+    public class WelcomMessageQuery : IRequest<string>
+    {
+        public string Name { get; set; }
+    }
+
+    public class WelcomMessageQueryHandler: IRequestHandler<WelcomMessageQuery, string>
+    {
+        public Task<string> Handle(WelcomMessageQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult($"Welcome {request.Name}");
+        }
+    }
+}
